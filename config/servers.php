35807f7b1ae06f9045fa177823179acdf1fb3215
wangyaxian <wangyaxiandev@gmail.com>
2021-04-07 14:51:05 +0800

    init

---
+++ config/servers.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'mode' => SWOOLE_PROCESS,
+    'http' => [
+        'ip' => '0.0.0.0',
+        'port' => 9502,
+        'sock_type' => SWOOLE_SOCK_TCP,
+        'settings' => [
+            'worker_num' => swoole_cpu_num(),
+        ],
+    ],
+    'ws' => [
+        'ip' => '0.0.0.0',
+        'port' => 9512,
+        'sock_type' => SWOOLE_SOCK_TCP,
+        'settings' => [
+            'worker_num' => swoole_cpu_num(),
+            'open_websocket_protocol' => true,
+        ],
+    ],
+];
