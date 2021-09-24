0bea1c1165f2e2fa4d123e81a07cf0c216773bb8
wangyaxian <wangyaxiandev@gmail.com>
2021-09-24 16:08:44 +0800

    init

--- app/Module/Index/Action/IndexAction.php
+++ app/Module/Index/Action/IndexAction.php
@@ -19,7 +19,7 @@ class IndexAction
         // 投递一个测试 Task
         // HttpServer::deliveryTask(TaskConstant::TASK_NAME_TEST, ['key' => 'val']);
 
-        $db = MySQLUtil::getInstance()->getConnection('127.0.0.1', 'root1', 'WYX*wyx123', 'account');
+        $db = MySQLUtil::getInstance()->getConnection('127.0.0.1', 'root', 'WYX*wyx123', 'account');
 
         $list = $db->search('t_user');
 
